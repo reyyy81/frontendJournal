f717b7b0d3bfaf3706526802cd2a28e91db46ff2
FeyiAdeaga <feyithereal@gmail.com>
2022-11-04 00:05:35 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 import Blogs from './components/pages/Blogs';
 import NewsCard from './components/pages/NewsCard';
+import Navbar from './components/navbar'
+import Home from './components/pages/Home';
 
 
 
 function App() {
   return (
-    <div classNameName="App">
-      Very Slay Behavior by react
-      <Blogs/>
-      <NewsCard/>
-    </div>
+    <>
+    <Navbar/>
+    <Router>
+      <Routes>
+        <Route index element={<Home/>}></Route>
+        <Route path='blogs' element={<Blogs/>}></Route>
+        <Route path='news' element={<NewsCard/>}></Route>
+      </Routes>
+    </Router>
+
+    </>
   );
 }
 
