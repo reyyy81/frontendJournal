a62dc8ea8c03e9e566c9484f273fe0035d6b1bca
FeyiAdeaga <feyithereal@gmail.com>
2022-11-08 14:52:41 -0600

    Added blue navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router , Routes, Route, Link} from 'react-router-dom';
 import Blogs from './components/pages/Blogs';
 import NewsCard from './components/pages/NewsCard';
 import Navbar from './components/navbar'
@@ -11,11 +11,12 @@ import Home from './components/pages/Home';
 function App() {
   return (
     <>
-    <Navbar/>
     <Router>
+    <Navbar/>
       <Routes>
         <Route index element={<Home/>}></Route>
         <Route path='blogs' element={<Blogs/>}></Route>
+        <Route path='home' element={<Home/>}></Route>
         <Route path='news' element={<NewsCard/>}></Route>
       </Routes>
     </Router>
