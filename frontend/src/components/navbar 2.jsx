042a7b716ebb9f8c4b679a9030b3fd578680d66b
FeyiAdeaga <feyithereal@gmail.com>
2022-11-26 13:04:17 -0600

    Fixed conflict

---
+++ frontend/src/components/navbar 2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <Link to="/home"><div className='nav-button'>Home</div></Link>
+      <Link to="/blogs"><div className='nav-button'>Blogs</div></Link>
+      <Link to="/news"><div className='nav-button'>News</div></Link>
+      <Link to="/art"><div className='nav-button'>Art</div></Link>
+      <Link to="/blogs"><div className='nav-button'>Posts</div></Link>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/components/pages/Cards 2.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import './Cards.css'
+
+
+const Cards = () => {
+  return (
+    <div>
+        <div className="card-grid">
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media4.giphy.com/media/cfuL5gqFDreXxkWQ4o/200w.webp?cid=ecf05e470eg7h541ajh7gzoaa1lmmsvg0bygh164j3eakcfh&rid=200w.webp&ct=g" />
+      </div>
+      <div className="card-body">
+          <div className="card-body-text">
+          Car based Infrastructure
+          <p>By Chris Martin</p>
+          </div>  
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read more</button>
+      </div>
+    </div>
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media4.giphy.com/media/cGyBZ4gesbICuyMmlr/giphy.gif?cid=ecf05e47h4cenj0mq6kk3n4a06tuad5a9m6dztxacy90kvv0&rid=giphy.gif&ct=g" />
+      </div>
+      <div className="card-body">
+        <div className="card-body-text">
+            A guide to survive Fall in Nashville 
+          <p>By Phoebe Bridgers</p>
+        </div>   
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read More</button>
+      </div>
+    </div>
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media2.giphy.com/media/ohKsuoQwOdmmGubVJm/200w.webp?cid=ecf05e47g15o45ozi3lt3tssmv68uapqvujiewryd85ez2ta&rid=200w.webp&ct=g" />
+      </div>
+      <div className="card-body">
+      <div className="card-body-text">
+            My Plant's dying
+          <p>By Rachel Green</p>
+        </div> 
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read More</button>
+      </div>
+    </div>
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media2.giphy.com/media/l2JecXTtDTmOMSUJG/200w.webp?cid=ecf05e47cez7kgmjdwspi80mnw2ifdd5671pvae3zti7dcz8&rid=200w.webp&ct=g" />
+      </div>
+      <div className="card-body">
+        <div className="card-body-text">
+        HoCo through my eyes
+        <p>By Micheael Scott</p>
+        </div> 
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read More</button>
+        
+      </div>
+    </div>
+
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media4.giphy.com/media/l3V0gnmiNvCNz85Wg/giphy.gif?cid=ecf05e47uxzfxiu2pkv78l4bekvg71ou7zgysrzgfq2f8d74&rid=giphy.gif&ct=g" />
+      </div>
+      <div className="card-body">
+          <div className="card-body-text">
+          Reminder: Invest money! 
+          <p>By Thomas Shelby</p>
+          </div>  
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read more</button>
+      </div>
+    </div>
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media2.giphy.com/media/gKtPBbunYNZjuFYPY1/giphy.gif?cid=ecf05e47brkn0pw441qxn7akxgp76f9t8jl8cmp89tm3dea2&rid=giphy.gif&ct=g" />
+      </div>
+      <div className="card-body">
+        <div className="card-body-text">
+            Love in the time of inflation 
+          <p>By Ted Mosby</p>
+        </div>   
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read More</button>
+      </div>
+    </div>
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media3.giphy.com/media/amUVFzg1wNZKg/giphy.gif?cid=ecf05e47vmtmsotw5ea4wvpsi6y94m8ojaf6g0ui23e26akk&rid=giphy.gif&ct=g" />
+      </div>
+      <div className="card-body">
+      <div className="card-body-text">
+            How to ace your midterms?
+          <p> By Lorelai Gilmore</p>
+        </div> 
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read More</button>
+      </div>
+    </div>
+    <div className="card card-shadow">
+      <div className="card-header card-image">
+        <img src="https://media0.giphy.com/media/QKAYhTkgisr94qhqit/giphy.gif?cid=ecf05e47dytonkgccl7t95wdsbyw6zrgiphxq46rqf124anm&rid=giphy.gif&ct=g" />
+      </div>
+      <div className="card-body">
+        <div className="card-body-text">
+        Overuse of Slay
+        <p>By Awantika Bastola</p>
+        </div> 
+      </div>
+      <div className="card-footer">
+        <button className="btn">Read More</button>
+        
+      </div>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default Cards
